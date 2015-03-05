4bd3e95123a58e297e11705e9bfdbaff0391c31b
Jordan Grieve <quintilex@gmail.com>
2015-03-05 12:01:30 -0500

    removed unused nav

--- src/navigation.php
+++ src/navigation.php
@@ -5,6 +5,7 @@ use DealerLive\Core\Classes\NavigationManager;
 
 $navigation = NavigationManager::find('Reporting');
 
+/*
 $child = new NavigationItem('Notifications');
 $child->setURL(\URL::to('dashboard/actionlog/errors/notifications'));
 $child->setPackage('Action Log');
@@ -18,4 +19,4 @@ $navigation->addChild($child);
 $child = new NavigationItem('View Page Data');
 $child->setURL(\URL::to('dashboard/actionlog/report/pages'));
 $child->setPackage('Action Log');
-$navigation->addChild($child);
+$navigation->addChild($child);*/
